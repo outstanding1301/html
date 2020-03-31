f224eefa5015084d84b74cc535571d0efcef3b5d
outstandingboy <outstandingboy@kakao.com>
2020-03-31 16:21:37 +0900

    Updates

--- precache-manifest.7a307ef516b0399677686d014b31d4c8.js
+++ precache-manifest.7a307ef516b0399677686d014b31d4c8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "06f14e0bef5fe6811274bfd8fabe30b1",
+    "revision": "44e379fceea3f7c1b92a746b55501cca",
     "url": "/html-generator/index.html"
   },
   {
-    "revision": "42f18a0a6cee230c809d",
+    "revision": "cf9dde014c23f89b91f9",
     "url": "/html-generator/static/css/main.bf9850ac.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/html-generator/static/js/2.0258d958.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "42f18a0a6cee230c809d",
-    "url": "/html-generator/static/js/main.f5c32739.chunk.js"
+    "revision": "cf9dde014c23f89b91f9",
+    "url": "/html-generator/static/js/main.026b8f15.chunk.js"
   },
   {
     "revision": "cbe9c7f65d67b324072a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/html-generator/precache-manifest.6ef0a695e624d5feafd4c17af8add730.js"
+  "/html-generator/precache-manifest.7a307ef516b0399677686d014b31d4c8.js"
 );
 
 self.addEventListener('message', (event) => {
